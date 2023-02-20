a8f47d2216b011b31afa9349989483ca423466ae
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-02-20 20:57:26 +0200

    This is Tuple selection

---
+++ tuple.py
@@ -0,0 +1,10 @@
+mylist = ["apple", "banana", "cherry"]
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
