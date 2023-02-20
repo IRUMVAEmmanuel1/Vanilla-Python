7c4851b70aa6b4217664181b343f617b021b988c
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-02-20 20:47:17 +0200

    First VANILLA CODES

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Hello there This is Vanilla Javascript")
 
 name=str(input("Enter your name"))
+
+def animals():
+    x = 4       # x is of type int
+    x = "Sally" # x is now of type str
+    print(x)
