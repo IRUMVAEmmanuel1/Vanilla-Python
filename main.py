5b496c3ca241c716c44f33d49b2be255bbc1d38f
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-02-20 20:49:58 +0200

    Numbers VANILLA CODES

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def animals():
     x = 4       # x is of type int
     x = "Sally" # x is now of type str
     print(x)
+
+def numbers():
+    x = 1
+    y = 35656222554887711
+    z = -3255522
+
+    print(type(x))
+    print(type(y))
+    print(type(z))
